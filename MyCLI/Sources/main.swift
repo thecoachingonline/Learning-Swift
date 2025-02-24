5badbe68e84a52197b5d6ca0c2372bd7c1df8abf
Y.Nanthachai <thecoachingonline@gmail.com>
2025-02-24 02:11:26 +0000

    Values

--- MyCLI/Sources/main.swift
+++ MyCLI/Sources/main.swift
@@ -3,3 +3,17 @@
 // https://swiftlang.github.io/swiftly/
 
 print("🙏 สวัสดีชาวโลก! ☸️")
+
+
+var fruits = ["strawberries", "limes", "tangerines"]
+fruits[1] = "grapes"
+
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+ ]
+occupations["Jayne"] = "Public Relations"
+
+fruits.append("blueberries")
+print(fruits)
