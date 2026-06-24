b04bc17d52b3df83f8a44dded8cb229e2fb816c3
Y.Nanthachai <thecoachingonline@gmail.com>
2026-06-24 03:35:46 +0000

    Multiple Variables

---
+++ Basics/Variables/MultipleVariables/Sources/MultipleVariables/MultipleVariables.swift
@@ -0,0 +1,15 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct MultipleVariables {
+    static func main() {
+        var x = 1, y = 2, z = 3
+        let a = 10, b = 20
+        print(x, y, z, a, b)
+
+        var i: Int = 1, j: Int = 2
+        let firstName: String = "Robin", lastName: String = "Refsnes"
+        print(i + j, firstName, lastName)
+    }
+}
