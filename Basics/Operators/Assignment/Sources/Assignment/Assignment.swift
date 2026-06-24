bf9a05f4a7b30acdfde23f3cbe8d6b92aa9b2873
Y.Nanthachai <thecoachingonline@gmail.com>
2026-06-24 06:41:27 +0000

    Assignment

---
+++ Basics/Operators/Assignment/Sources/Assignment/Assignment.swift
@@ -0,0 +1,21 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct Assignment {
+    static func main() {
+        var total = 10
+        total += 5
+        print(total)
+        total -= 3
+        print(total)
+        total *= 2
+        print(total)
+        total /= 4
+        print(total)
+
+        var s = "Hello"
+        s += ", Swift"
+        print(s)
+    }
+}
