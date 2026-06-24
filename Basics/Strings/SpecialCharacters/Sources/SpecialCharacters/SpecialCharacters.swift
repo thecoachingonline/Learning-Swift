023955ea64c5db05533046320d74711d5c10a7f3
Y.Nanthachai <thecoachingonline@gmail.com>
2026-06-24 07:12:31 +0000

    Special  Characters

---
+++ Basics/Strings/SpecialCharacters/Sources/SpecialCharacters/SpecialCharacters.swift
@@ -0,0 +1,18 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct SpecialCharacters {
+    static func main() {
+        print("Hello\nSwift")
+        print("A\tB\tC")
+        print("\"quoted\"")
+        print("\\")
+
+        let poem = """
+        Roses are red,
+        Violets are blue.
+        """
+        print(poem)
+    }
+}
