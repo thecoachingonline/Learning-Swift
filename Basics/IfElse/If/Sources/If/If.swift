829f8e25a00c9eb0c4b83259e91446dafff9311c
Y.Nanthachai <thecoachingonline@gmail.com>
2026-06-24 08:02:07 +0000

    If

---
+++ Basics/IfElse/If/Sources/If/If.swift
@@ -0,0 +1,17 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct If {
+    static func main() {
+        let temp = 25
+        if temp > 20 {
+            print("Warm")
+        }
+
+        let speed = 55
+        if speed > 50 {
+            print("Slow down")
+        }
+    }
+}
