34a58d3f620b7dda91229a012c05c1139834f5b7
Y.Nanthachai <thecoachingonline@gmail.com>
2026-06-24 03:12:13 +0000

    Comments

---
+++ Basics/Comments/Sources/Comments/Comments.swift
@@ -0,0 +1,17 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct Comments {
+    static func main() {
+        // This is a single-line comment
+        print("Hello")
+        
+        /* This is a multi-line comment */
+        print("Swift")
+        
+        /* Nested comments are allowed:
+            /* inner */
+        */
+    }
+}
