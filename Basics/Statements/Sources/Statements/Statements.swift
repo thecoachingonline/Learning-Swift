877d6882c0cc49aeec5b425559d7ab0e11ecb10e
Y.Nanthachai <thecoachingonline@gmail.com>
2026-06-24 02:55:19 +0000

    Statements

---
+++ Basics/Statements/Sources/Statements/Statements.swift
@@ -0,0 +1,14 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct Statements {
+    static func main() {
+        let a = 1; let b = 2 // semicolons can separate multiple statements on one line
+           print(a + b)
+           
+        if a < b {
+            print("a is less than b")
+        }
+    }
+}
