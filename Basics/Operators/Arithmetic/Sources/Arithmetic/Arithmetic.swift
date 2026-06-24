b720d27742191d4ec3747c7199ee81a10a0d1d32
Y.Nanthachai <thecoachingonline@gmail.com>
2026-06-24 06:25:51 +0000

    Arithmetic

---
+++ Basics/Operators/Arithmetic/Sources/Arithmetic/Arithmetic.swift
@@ -0,0 +1,16 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct Arithmetic {
+    static func main() {
+        let a = 7, b = 3
+        print(a - b)
+        print(a * b)
+        print(a / b)        // integer division
+
+        print(7 % 3)   // 1
+        print(8 % 2)   // 0
+        print(10 % 6)  // 4
+    }
+}
