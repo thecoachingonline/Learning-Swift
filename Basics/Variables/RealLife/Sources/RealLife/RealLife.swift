598f1b21373def276ee630078ce78f869736e883
Y.Nanthachai <thecoachingonline@gmail.com>
2026-06-24 04:13:54 +0000

    Real Life

---
+++ Basics/Variables/RealLife/Sources/RealLife/RealLife.swift
@@ -0,0 +1,16 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct RealLife {
+    static func main() {
+        var count = 0
+        count += 1
+        print("Count: \(count)")
+
+        var name = "Kai"
+        print("Hello, \(name)!")
+        name = "Elisabeth"
+        print("Hello, \(name)!")
+    }
+}
