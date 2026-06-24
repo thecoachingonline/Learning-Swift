713344dd46f5979d5336ca3ccd16ba9a4011fdd5
Y.Nanthachai <thecoachingonline@gmail.com>
2026-06-24 02:45:07 +0000

    Syntax

---
+++ Basics/Syntax/Sources/Syntax/Syntax.swift
@@ -0,0 +1,15 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct Syntax {
+    static func main() {
+        let greeting = "Hello"
+        var name = "Swift"
+        print(greeting + ", " + name)
+        
+        let pi: Double = 3.14159
+        var count: Int = 3
+        print("pi = \(pi), count = \(count)")
+    }
+}
