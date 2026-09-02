cdf501c4e40fbe20d5f086d529c6b63dbb6ce075
Y.Nanthachai <thecoachingonline@gmail.com>
2026-09-02 11:00:10 +0000

    Swift for Android

---
+++ Android/hello/Sources/hello/hello.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct hello {
+    static func main() {
+        print("Hello, world! Swift SDK for Android")
+    }
+}
